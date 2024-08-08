944640553be3ed6a797e5df1f96b8304a4c91e95
Suvansh7 <jindalsuvansh7@gmail.com>
2024-08-08 11:59:18 +0530

    y

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI
 import llm
 app = FastAPI()
 
-@app.get("/get")
-def display(name: str):
+@app.get("/")
+def display():
     return{"Message":f"Hello {llm.testing()}"}
 
-if __name__ == "__main__":
-    app()
